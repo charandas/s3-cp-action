84d2ae3dc81332ef0f28fdf6b2df6c5dc9882be0
Charandas <charandas@users.noreply.github.com>
2021-07-06 12:54:57 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ fi
 # Create a dedicated profile for this action to avoid conflicts
 # with past/future actions.
 # https://github.com/jakejarvis/s3-cp-action/issues/1
-aws configure --profile s3-cp-action <<-EOF > /dev/null 2>&1
+if [ -n "INPUT_AWS_ACCESS_KEY_ID" ]; then
+  aws configure --profile s3-cp-action <<-EOF > /dev/null 2>&1
 ${INPUT_AWS_ACCESS_KEY_ID}
 ${INPUT_AWS_SECRET_ACCESS_KEY}
 ${INPUT_AWS_REGION}
