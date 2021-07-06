247ff85e56142e4644a8fd7a38b1b6cb6951dea2
Charandas <charandas@users.noreply.github.com>
2021-07-06 12:56:31 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,6 @@ EOF
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 cp ${INPUT_SOURCE} ${INPUT_DEST} \
-              --profile s3-cp-action \
               --no-progress \
               ${ENDPOINT_APPEND} ${INPUT_FLAGS} $*"
 
